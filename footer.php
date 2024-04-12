d5b04d6c18ae080856b3bd64850bb5809e399493
IgorMan <136973037+IgorMan2005@users.noreply.github.com>
2024-04-12 22:30:31 +0300

    Impact PHP - Bootstrap Business Website Template

---
+++ footer.php
@@ -0,0 +1,90 @@
+
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+
+    <div class="container">
+        <div class="row gy-4">
+            <div class="col-lg-5 col-md-12 footer-info">
+                <a href="index.html" class="logo d-flex align-items-center">
+                    <span>Impact</span>
+                </a>
+                <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+                <div class="social-links d-flex mt-4">
+                    <a target="_blank" href="<?= $config['vk'] ?>" class="vk"><i class="fa fa-vk" aria-hidden="true"></i></a>
+                    <a target="_blank" href="<?= $config['twitter'] ?>" class="twitter"><i class="bi bi-twitter"></i></a>
+                    <a target="_blank" href="<?= $config['facebook'] ?>" class="facebook"><i class="bi bi-facebook"></i></a>
+                    <a target="_blank" href="<?= $config['instagram'] ?>" class="instagram"><i class="bi bi-instagram"></i></a>
+                    <a target="_blank" href="<?= $config['linkedin'] ?>" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-6 footer-links">
+                <h4>Useful Links</h4>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Terms of service</a></li>
+                    <li><a href="#">Privacy policy</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2 col-6 footer-links">
+                <h4>Our Services</h4>
+                <ul>
+                    <li><a href="#">Web Design</a></li>
+                    <li><a href="#">Web Development</a></li>
+                    <li><a href="#">Product Management</a></li>
+                    <li><a href="#">Marketing</a></li>
+                    <li><a href="#">Graphic Design</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+                <h4>Contact Us</h4>
+                <p>
+                    <?= $config['address'] ?>
+                    <br><br>
+                    <strong>Phone:</strong> <a href="tel:<?= phone_link($config['phone']) ?>">
+                        <span style="color: #ffffff;"><?= $config['phone'] ?></span></a> <br>
+                    <strong>Email:</strong> <a href="mailto:<?= $config['email'] ?>">
+                        <span style="color: #ffffff;"><?= $config['email'] ?></span></a>
+                    <br>
+                </p>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container mt-4">
+        <div class="copyright">
+            &copy; Copyright <strong><span><?= $config['title'] ?></span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            PHP version created by <a href="https://best-itpro.ru">Best IT Pro</a>
+        </div>
+    </div>
+
+</footer><!-- End Footer -->
+<!-- End Footer -->
+
+<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
